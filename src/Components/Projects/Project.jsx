ea94c14aed5285ded10f8622810876ae2dda07fe
Anis Mulla <135443188+AnisMulla9740@users.noreply.github.com>
2025-05-03 01:26:53 +0530

    Update Project.jsx

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -56,7 +56,7 @@ const vlsiProjects = [
     tech: ['Verilog', 'Vivado', 'Microwind 3.9', 'DSCH'],
     github: 'https://github.com/AnisMulla9740/8_Bit_Booth_Multiplier_using_120nm_scale_technology',
     link: '#',
-    image: 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/...',
+    image: 'https://www.google.com/url?sa=i&url=http%3A%2F%2Fvlabs.iitkgp.ac.in%2Fcoa%2Fexp7%2Findex.html&psig=AOvVaw3HcIybv3w9m9QwmgFHOr7J&ust=1746302148088000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMj-n_nHhY0DFQAAAAAdAAAAABAJ',
     objectPosition: 'center'
   },
   {
